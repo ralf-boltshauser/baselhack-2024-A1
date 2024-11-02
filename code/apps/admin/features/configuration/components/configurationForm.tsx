fc41f8dd31b653454b5146503ad847b982a2e84d
Lionel Ding <lionel.ding@hotmail.ch>
2024-11-02 17:52:11 +0100

    🗃️ Database seeding and scoring fields rework

---
+++ code/apps/admin/features/configuration/components/configurationForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { Input } from "@repo/ui/components/ui/input";
+import ConfigurationChart from "./configurationChart";
+import { Button } from "@repo/ui/components/ui/button";
+import { AttributeName } from "@repo/db";
+import { useState } from "react";
+
+function QuestionWeight({
+  question,
+  value,
+  onChange,
+}: {
+  question: string;
+  value: number;
+  onChange: (e: any) => void;
+}) {
+  return (
+    <div className="flex flex-col items-start justify-between gap-1">
+      <p>{question}</p>
+      <Input type="number" className="w-24" value={value} onChange={onChange} />
+    </div>
+  );
+}
+
+type Weights = {
+  [x in AttributeName]: number;
+};
+
+export default function ConfigurationForm({
+  currentWeights,
+}: {
+  currentWeights: Weights;
+}) {
+  const [weights, setWeights] = useState<Weights>(currentWeights);
+
+  return (
+    <div className="grid grid-cols-2">
+      <div className="flex flex-col">
+        <div className="mb-4">
+          <h2 className="text-xl font-semibold">Adjust weights</h2>
+          <p className="text-sm text-foreground">
+            Higher weights means lower risk appetite.
+          </p>
+        </div>
+        <div className="flex flex-col gap-y-4">
+          <QuestionWeight
+            question="Are you smoking ?"
+            value={weights.SMOKING}
+            onChange={(e) =>
+              setWeights({ ...weights, SMOKING: e.target.value })
+            }
+          />
+          <QuestionWeight
+            question="How hold are you ?"
+            value={weights.AGE}
+            onChange={(e) => setWeights({ ...weights, AGE: e.target.value })}
+          />
+          <QuestionWeight
+            question="What's your BMI ?"
+            value={weights.BMI}
+            onChange={(e) => setWeights({ ...weights, BMI: e.target.value })}
+          />
+          <QuestionWeight
+            question="How long the death insurance lasts ?"
+            value={weights.DURATION}
+            onChange={(e) =>
+              setWeights({ ...weights, DURATION: e.target.value })
+            }
+          />
+          <QuestionWeight
+            question="How much the insurance sum is ?"
+            value={weights.INSURANCE_SUM}
+            onChange={(e) =>
+              setWeights({ ...weights, INSURANCE_SUM: e.target.value })
+            }
+          />
+          <QuestionWeight
+            question="Have you had cardiac issues ?"
+            value={weights.CARDIAC_ISSUES}
+            onChange={(e) =>
+              setWeights({ ...weights, CARDIAC_ISSUES: e.target.value })
+            }
+          />
+          <QuestionWeight
+            question="Are you diabetic ?"
+            value={weights.DIABETIC_CONDITION}
+            onChange={(e) =>
+              setWeights({ ...weights, DIABETIC_CONDITION: e.target.value })
+            }
+          />
+          <QuestionWeight
+            question="Do you suffer from hypertension ?"
+            value={weights.HYPERTENSION}
+            onChange={(e) =>
+              setWeights({ ...weights, HYPERTENSION: e.target.value })
+            }
+          />
+          <Button variant="default" className="w-52">
+            Save and apply
+          </Button>
+        </div>
+      </div>
+      <div>
+        <div className="mb-4">
+          <h2 className="text-xl font-semibold">Visual representation</h2>
+          <p className="text-sm text-foreground">
+            This chart shows the risk appetite of your insurance pool.
+          </p>
+        </div>
+        <ConfigurationChart chartData={[]} />
+      </div>
+    </div>
+  );
+}
--- code/apps/admin/features/configuration/components/configurationPage.tsx
+++ code/apps/admin/features/configuration/components/configurationPage.tsx
@@ -1,9 +1,16 @@
+import ConfigurationForm from "./configurationForm";
+
 export default async function ConfigurationPage() {
   return (
     <div className="w-full">
-      <div className="flex justify-between items-center bg-background mb-8">
+      <div className="flex justify-start items-start bg-background mb-8 flex-col gap-y-2">
         <h1 className="text-3xl">Configurations</h1>
+        <p className="text-sm text-foreground">
+          Adjust the multipliers to align the risk appetite you're comfortable
+          with in your insurance pool.
+        </p>
       </div>
+      <ConfigurationForm currentWeights={undefined} />
     </div>
   );
 }
